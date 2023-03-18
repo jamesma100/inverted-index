d6bb6b104e63364e46e54f14e4aa2be731244b3c
James Ma <jamesma100@gmail.com>
2023-03-18 15:05:33 -0700

    clean stdout, update var names

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::fs;
 use std::collections::HashMap;
 
 fn main() {
-    let mut word_pos = HashMap::new();
+    let mut wordmap = HashMap::new();
     let args: Vec<String> = env::args().collect();
-    let dir_path = parse_config(&args);
+    let (dir_path, word) = parse_config(&args);
 
     let paths = fs::read_dir(dir_path)
         .unwrap()
@@ -14,39 +14,45 @@ fn main() {
         .collect::<Vec<_>>();
 
     for path in paths {
-        let path_clone = path.clone();
-        let contents: String = fs::read_to_string(path)
+        let contents: String = fs::read_to_string(&path)
             .expect("Error reading file");
 
         for line in contents.lines() {
             let splitline = line.split([' ', ',', '.', '?', '!']);
             for word in splitline {
                 if !word.is_empty() {
-                    insert_into_hm(&mut word_pos, word, &path_clone); 
+                    insert_into_hm(&mut wordmap, word, &path); 
                 }
             }
         }
     }
-    // info(&mut word_pos);
-    // get_occurences(&mut word_pos, "well");
+    sort_and_dedup(&mut wordmap);
+    get_occurences(&mut wordmap, word);
 }
 
-fn parse_config(args: &[String]) -> String {
-    let file_path = args[1].clone();
-    file_path
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let file_path = &args[1];
+    let word = &args[2]; 
+    (file_path, word)
 }
 
 fn insert_into_hm(hm: &mut HashMap<String, Vec<String>>, word: &str, doc: &String) {
     hm.entry(word.to_owned()).or_insert(Vec::new()).push(doc.to_owned());
 }
 
-fn info(hm: &mut HashMap<String, Vec<String>>) {
-    for (word, pos) in hm {
-        println!("{}: {}", word, pos.len());
+fn sort_and_dedup(hm: &mut HashMap<String, Vec<String>>) {
+    for (_, pos) in hm {
+        pos.sort();
+        pos.dedup();
     }
 }
 
 fn get_occurences(hm: &mut HashMap<String, Vec<String>>, word: &str) {
-    let docs = hm.get(word);
-    println!("{:?}", docs); 
+    let opt = hm.get(word);
+    if !opt.is_none() {
+        let docs = opt.unwrap();
+        for doc in docs {
+            println!("{}", doc);
+        }
+    }
 }
